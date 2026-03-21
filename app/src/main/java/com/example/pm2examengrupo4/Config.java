bf3c0a696f9e727a6b17b68e7871bbb7ca1300b1
Enrique Jared <emncrft01@hotmail.com>
2026-03-21 11:51:21 -0600

    maps echo, video sin funcionar

--- app/src/main/java/com/example/pm2examengrupo4/Config.java
+++ app/src/main/java/com/example/pm2examengrupo4/Config.java
@@ -1,5 +1,5 @@
 package com.example.pm2examengrupo4;
 
 public class Config {
-        public static final String BASE_URL = "http://192.168.18.207:3000/";
+        public static final String BASE_URL = "http://10.122.46.103:3000/";
 }
